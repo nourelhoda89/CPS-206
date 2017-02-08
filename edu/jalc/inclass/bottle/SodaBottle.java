6186ac20e28f39de57b1cb0030b682c17c8e4e4f
E231 <E231@volnet.jalcdom.jal.cc.il.us>
2017-02-08 08:49:11 -0600

    2/8

--- edu/jalc/inclass/bottle/SodaBottle.java
+++ edu/jalc/inclass/bottle/SodaBottle.java
@@ -2,9 +2,12 @@ package edu.jalc.inclass.bottle;
 ////not specified then its package private only the package can access
 public class SodaBottle {
 
-	private final double capacity = 20;
+	private double capacity;
 	//this method allow SodaBottleApp to access capacity
 	public double getCapacity(){
+		SetCapacity(20.0);
 		return capacity;
 	}
+		public void SetCapacity(double capacity){
+			this.capacity =capacity;
 }
