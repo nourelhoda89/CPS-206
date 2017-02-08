26f9e690a8796109910171521f1f15533563e626
nourelhoda89 <nfarhat6558@volmail.jalc.edu>
2017-02-08 15:17:12 -0600

    new

--- edu/jalc/inclass/bottle/SodaBottleApp.java
+++
@@ -1,9 +0,0 @@
-package edu.jalc.inclass.bottle;
-
-public class SodaBottleApp {
-
-	public static void main(String... args){
-		SodaBottle sodaBottle = new SodaBottle();
-		System.out.println("Capacity: " + sodaBottle.getCapacity());
-	}
-}
