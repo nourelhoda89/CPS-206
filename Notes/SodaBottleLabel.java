df4d906ea8e9c531a59d916757805e4c58359465
E231 <E231@volnet.jalcdom.jal.cc.il.us>
2017-02-09 08:49:59 -0600

    label

---
+++ Notes/SodaBottleLabel.java
@@ -0,0 +1,37 @@
+package edu.jalc.inclass.label;
+
+public class SodaBottleLabel{
+
+
+
+    private double lableHeight;
+    private  Color color;
+    private byte blue;
+    private byte green;
+    private byte  red;
+    private Logo logo;  //create object called color rgb values
+                        //create object called logo, primitive that represent logo
+
+    //private double lableCircumfreance;
+
+    private  SodaBottleLabel(){
+
+    }
+    private  SodaBottleLabel(){
+
+    }
+    private  SodaBottleLabel(){
+
+    }
+
+    public String getLabelType(){return this. labelType}
+    public boolean getLabelState(){return this.isLabelOn}
+    public double getLabelHeight(){return this.lableHeight}
+    public double getlableCircumfreance(){return this.lableCircumfreance}
+
+
+
+    private void getLabelState(){
+
+    }
+}
