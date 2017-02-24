d67a6a8d089778d71770bd930c4afc5e6a18e3f7
nourelhoda89 <nfarhat6558@volmail.jalc.edu>
2017-02-24 08:52:45 -0600

    Create Dog.java

---
+++ animal/Dog.java
@@ -0,0 +1,20 @@
+package exercises.polymorphism.animal;
+
+public class Dog extends Animal {
+
+   String bark;
+   String whimper;
+
+   private Dog(){
+      super(null);
+   }
+
+   public Dog(String bark, String growl, String whimper){
+      super(growl);
+      this.bark = bark;
+      this.whimper = whimper;
+   }
+   public String toString (){
+      return this.getGrowl() + " " + this.bark + " " +this.whimper;
+   }
+}
